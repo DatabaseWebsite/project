94748e9c05e8df03430e965f6be29f6db34a6133
18504 <21371497@buaa.edu.cn>
2023-12-01 18:12:07 +0800

    show finish need more methods and create

--- frontend/src/components/course_management/courseForm.vue
+++ frontend/src/components/course_management/courseForm.vue
@@ -1,75 +1,108 @@
-<template>
-    <el-container class="card-container">
-    <el-card class="box-card">
-      <template #header>
-        <div class="card-header">
-          <span> {{ item.course_id }}
-          </span>
-          <el-button class="button" text>Operation button</el-button>
-        </div>
-      </template>
+<template>    
+  <el-container class="card-container">    
+    <el-card class="box-card">    
+      <template #header>    
+        <div class="card-header">    
+          <span>{{ item.course_id }}</span>    
+          <el-button text @click="dialogVisible = true">
+              详细信息
+           </el-button>  
+        </div>    
+      </template>    
+      <div class="text item">    
+        {{ item.name }}    
+      </div>    
+    </el-card>    
+  </el-container>    
+  <el-dialog
+    v-model="dialogVisible"
+    title="Tips"
+    width="30%"
+  >
+    <span>
       
-      <div class="text item">
-        {{ item.name }}
-      </div>
-    </el-card>
-    </el-container>
-  </template>
-  
-  <script lang="ts">
-  
-  
-  export default {
-    name: 'announceForm',
-    props: {
-      item: Object
+        <el-table :data="students" style="width: 100%">
+        <el-table-column prop="student_id" label="id" width="90" />
+        <el-table-column prop="name" label="Name" width="90" />
+        <el-table-column prop="identity" label="identity" />
+        </el-table>
+      
+    </span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click=cancelDialog()>Cancel</el-button>
+        <el-button type="primary" @click=confirmDialog()>
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>   
+</template>    
     
+<script lang="ts">    
+import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+const dialogVisible = ref(false)
+
+export default {    
+  name: 'courseForm',    
+  props: {    
+    item: Object,    
+  },    
+  data() {    
+    return {    
+      dialogVisible: false,    
+      dialogTitle: '课程参与信息',    
+      students: [{student_id:1,name:'gaosj',identity:'老师'},{student_id:2,name:'byc',identity:'学生'},] // 存储学生信息的数组，每个元素为 { student_id, name, identity, grade } 类型的对象    
+    };    
+  },    
+  mounted() {    
+    //this.loadStudents(); // 在组件挂载后加载学生信息    
+  },    
+  methods: {    
+    submit() {    
+      // 提交表单的逻辑在这里实现    
+    },    
+    loadStudents() {    
+      // 调用 API 或通过其他方式获取学生信息，并存储在 students 数组中    
+      // 例如：this.students = getStudentsFromAPI(this.item.course_id);    
+    },    
+    cancelDialog() { // 打开对话框的方法    
+      this.dialogVisible = false;// 检查值是否被正确修改为true
     },
-    data() {
-      return {
-        text: '',
-        ritem: ''
-      }
-    },
-    mounted() {
-      // 在生命周期钩子中更新子组件的数据
-      this.ritem = this.item;
-    },
-    methods: {
-      submit() {
-  
-      }
+    confirmDialog() {
+      this.dialogVisible = false;
     }
-    
-  };
-  </script>
-  <style scoped>
-  /* form */
-  /*:deep(.el-form-item__label)  {*/
-  /*  color: white; !* 设置字体颜色为白色 *!*/
-  /*}*/
-  .card-container{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .card-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+  }    
+};    
+</script>  
+<style scoped>  
+/* form */  
+/*:deep(.el-form-item__label)  {*/  
+/*  color: white; !* 设置字体颜色为白色 *!*/  
+/*}*/  
+.card-container{  
+  display: flex;  
+  justify-content: center;  
+  align-items: center;  
+}  
+.card-header {  
+  display: flex;  
+  justify-content: space-between;  
+  align-items: center;  
+}  
   
-  .text {
-    font-size: 14px;
-  }
+.text {  
+  font-size: 14px;  
+}  
   
-  .item {
-    margin-bottom: 18px;
-  }
+.item {  
+  margin-bottom: 18px;  
+}  
   
-  .box-card {
-    width: 90%;
-    padding:5px;
-    margin:10px;
-  }
-  </style>
+.box-card {  
+  width: 90%;  
+  padding:5px;  
+  margin:10px;  
+}  
+</style>
--- frontend/src/views/course_management.vue
+++ frontend/src/views/course_management.vue
@@ -1,47 +1,45 @@
-<template>
-    <div>1111
-        </div>
-    <el-button
-        @click="handleLogin()"
-        
-      >登录</el-button>
-    
-    <div>
-        <courseForm v-for="(item, index) in course_info" :item =item>
-        </courseForm>
-    </div>
-</template>
+<template>  
+  <el-container>   
+    <el-main>  
+      <courseForm v-for="(item, index) in course_info" :item="item" />  
+    </el-main>  
+  </el-container>  
+</template>  
   
-<script lang="ts">
-import { create_course_api, all_course_info_api } from "@/api/api.ts";
-import courseForm from "@/components/course_management/courseForm.vue";
-
-export default {
-  name: "course_management",
-  components: { courseForm },
-  data() {
-    return {
-      course_info: null,
-    };
-  },
-  methods: {
-    async handleLogin() {
-      try {
-        // Call the API
-        let info = await all_course_info_api();
-
-        // Update the data property
-        this.course_info = info.data.result;
-
-        console.log("API call successful");
-      } catch (error) {
-        console.error("Error calling API:", error);
-      }
-    },
-  },
-};
-</script>
-
-<style scoped>
-/* Your styles go here */
+<script lang="ts">  
+import { create_course_api, all_course_info_api } from "@/api/api.ts";  
+import courseForm from "@/components/course_management/courseForm.vue";  
+import { ElContainer, ElHeader, ElMain } from "element-plus";  
+  
+export default {  
+  name: "course_management",  
+  components: { courseForm },  
+  data() {  
+    return {  
+      course_info: null,  
+    };  
+  },  
+  created() {  
+    this.handleLogin(); // Call the login function during the creation of the component  
+  },  
+  methods: {  
+    async handleLogin() {  
+      try {  
+        // Call the API  
+        let info = await all_course_info_api();  
+  
+        // Update the data property  
+        this.course_info = info.data.result;  
+  
+        console.log("API call successful");  
+      } catch (error) {  
+        console.error("Error calling API:", error);  
+      }  
+    },  
+  },  
+};  
+</script>  
+  
+<style scoped>  
+/* Your styles go here */  
 </style>
